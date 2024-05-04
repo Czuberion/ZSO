68258305e4e68a1ab362e7f7b926f94811a117b1
Czuberion <czuberion@gmail.com>
2024-05-05 01:59:49 +0200

    Fixed thread control flow issues Added multi_problem_flag Made problem_count and its mutex debug only Replaced broadcasts for gates with signals

--- lotnisko.c
+++ lotnisko.c
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define PASSENGER_COUNT 10
+#define PASSENGER_COUNT 20
 #define GATE_COUNT 3
 #define CHOSEN_GATE_IDX id % GATE_COUNT
 
@@ -33,12 +33,16 @@ typedef struct {
 } Args;
 
 Gate gates[GATE_COUNT];
+
 pthread_mutex_t global_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t global_cond = PTHREAD_COND_INITIALIZER;
 int in_personal_check = 0; // Czy którykolwiek pasażer jest obecnie na kontroli osobistej
+int multi_problem_flag = 0;
 
+#ifdef DEBUG
 pthread_mutex_t prob_cnt_lock = PTHREAD_MUTEX_INITIALIZER;
 int problem_count = 0;
+#endif
 
 void* personal_check(void* arg) {
     volatile double result = (double)(*(int*)arg);
@@ -54,41 +58,47 @@ void* passenger_function(void* arg) {
     int chosen_gate_idx = CHOSEN_GATE_IDX;
     Gate* chosen_gate = &gates[chosen_gate_idx];
 
-    // Podejście do bramki
-    pthread_mutex_lock(&global_lock);
-    while(in_personal_check > 0) {
-        DEBUG_PRINT("Pasażer %d, bramka %d: czekanie na global\n", id, chosen_gate_idx);
-        pthread_cond_wait(&global_cond, &global_lock);
-    }
-    pthread_mutex_unlock(&global_lock);
     pthread_mutex_lock(&chosen_gate->lock);
-    while (!chosen_gate->is_active) { // helgrind data race 1, 2
-        DEBUG_PRINT("Pasażer %d, bramka %d: czekanie na gate\n", id, chosen_gate_idx);
+    //DEBUG_PRINT("Pasażer %d, bramka %d: [L][S] Lock bramki.\n", id, chosen_gate_idx);
+    while (!chosen_gate->is_active) {
+        DEBUG_PRINT("Pasażer %d, bramka %d: [W] czekanie na gate\n", id, chosen_gate_idx);
         pthread_cond_wait(&chosen_gate->condition, &chosen_gate->lock);
     }
 
+    pthread_mutex_lock(&global_lock);
+    //DEBUG_PRINT("Pasażer %d, bramka %d: [L][S] Lock global.\n", id, chosen_gate_idx);
+    while(in_personal_check > 0 && multi_problem_flag) {
+        DEBUG_PRINT("Pasażer %d, bramka %d: [W] czekanie na global\n", id, chosen_gate_idx);
+        pthread_cond_wait(&global_cond, &global_lock);
+    }
+
     DEBUG_PRINT("Pasażer %d, bramka %d: Podejście.\n", id, chosen_gate_idx);
-    
-    // Losowe wykrycie problemu przy przejściu przez bramkę
+
     if (IS_PROBLEM) {
+        chosen_gate->is_active = 0;
+
+        #ifdef DEBUG
         pthread_mutex_lock(&prob_cnt_lock);
         problem_count++;
         int this_prob = problem_count;
         pthread_mutex_unlock(&prob_cnt_lock);
+        #endif
+
         DEBUG_PRINT("Pasażer %d, bramka %d: [P%d][START] Problem przy prześwietlaniu.\n", id, chosen_gate_idx, this_prob);
-        
-        pthread_mutex_lock(&global_lock);
-        if (in_personal_check > 0) {
-            //DEBUG_PRINT("Pasażer %d: Inny pasażer na kontroli. Wstrzymanie bramek.\n", id);
+        if (in_personal_check > 0 && !multi_problem_flag) {
+            multi_problem_flag = 1;
             DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K>1][S] Już ktoś na kontroli. Wstrz. bram.\n", id, chosen_gate_idx, this_prob);
-            
+
             for (int i = 0; i < GATE_COUNT; i++) {
-                gates[i].is_active = 0; // helgrind data race 2
+                gates[i].is_active = 0;
                 DEBUG_PRINT("Pasażer %d, bramka %d: Wstrz. bram. %d\n", id, chosen_gate_idx, i);
             }
             DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K>1][S] Wstrz. wszyst. bram. zakończone.\n", id, chosen_gate_idx, this_prob);
         }
         in_personal_check++;
+
+        pthread_cond_broadcast(&global_cond);
+        //DEBUG_PRINT("Pasażer %d, bramka %d: [L][E] Unlock global.\n", id, chosen_gate_idx);
         pthread_mutex_unlock(&global_lock);
 
         // Kontrola osobista
@@ -97,32 +107,41 @@ void* passenger_function(void* arg) {
         pthread_create(&check, NULL, personal_check, &seed);
         pthread_join(check, NULL);
         DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K][E] Koniec kontroli.\n", id, chosen_gate_idx, this_prob);
-        
+
         pthread_mutex_lock(&global_lock);
+        //DEBUG_PRINT("Pasażer %d, bramka %d: [L][S] Lock global.\n", id, chosen_gate_idx);
         in_personal_check--;
 
-    	if (IS_PROBLEM_2ND) {
-       	    DEBUG_PRINT("Pasażer %d, bramka %d: Kontrola osobista wykryła problem. Pasażer nie wpuszczony na pokład.\n", id, chosen_gate_idx);
-	} else {
-	    DEBUG_PRINT("Pasażer %d, bramka %d: Kontrola osobista nie wykryła problemu. Pasażer wpuszczony na pokład.\n", id, chosen_gate_idx);
-	}
-        if (in_personal_check == 0) {
+        if (IS_PROBLEM_2ND) {
+            DEBUG_PRINT("Pasażer %d, bramka %d: Kontrola osobista wykryła problem. Pasażer nie wpuszczony na pokład.\n", id, chosen_gate_idx);
+        } else {
+            DEBUG_PRINT("Pasażer %d, bramka %d: Kontrola osobista nie wykryła problemu. Pasażer wpuszczony na pokład.\n", id, chosen_gate_idx);
+        }
+        if (in_personal_check == 0 && multi_problem_flag) {
             DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K>1][E] Nie ma więcej kontroli. Aktywacja bramek.\n", id, chosen_gate_idx, this_prob);
             for (int i = 0; i < GATE_COUNT; i++) {
-                gates[i].is_active = 1; // helgrind data race 1
-                DEBUG_PRINT("Pasażer %d, bramka %d: Aktyw. bram. %d\n", id, chosen_gate_idx, i);
-                pthread_cond_broadcast(&gates[i].condition); // pthread_cond_{signal,broadcast}: dubious: associated lock is not held by any thread
+                if(&gates[i] != chosen_gate) {
+                    gates[i].is_active = 1;
+                    DEBUG_PRINT("Pasażer %d, bramka %d: Aktyw. bram. %d\n", id, chosen_gate_idx, i);
+                    pthread_cond_signal(&gates[i].condition);
+                }
             }
+            DEBUG_PRINT("Pasażer %d, bramka %d: Aktyw. bram. %d\n", id, chosen_gate_idx, chosen_gate_idx);
             DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K>1][E] Aktyw. wszyst. bram. zakończone.\n", id, chosen_gate_idx, this_prob);
-            pthread_cond_broadcast(&global_cond);
+            multi_problem_flag = 0;
         }
+        chosen_gate->is_active = 1;
         DEBUG_PRINT("Pasażer %d, bramka %d: [P%d][END] Koniec problemów.\n", id, chosen_gate_idx, this_prob);
-        pthread_mutex_unlock(&global_lock);
     } else {
         DEBUG_PRINT("Pasażer %d, bramka %d: Przejście bez problemów.\n", id, chosen_gate_idx);
     }
 
+    pthread_cond_signal(&chosen_gate->condition);
+    pthread_cond_broadcast(&global_cond);
+    //DEBUG_PRINT("Pasażer %d, bramka %d: [L][E] Unlock bramki.\n", id, chosen_gate_idx);
     pthread_mutex_unlock(&chosen_gate->lock);
+    //DEBUG_PRINT("Pasażer %d, bramka %d: [L][E] Unlock global.\n", id, chosen_gate_idx);
+    pthread_mutex_unlock(&global_lock);
 
     return NULL;
 }
@@ -163,7 +182,9 @@ int main() {
     cleanup();
     pthread_mutex_destroy(&global_lock);
     pthread_cond_destroy(&global_cond);
-
+    #ifdef DEBUG
+    pthread_mutex_destroy(&prob_cnt_lock);
+    #endif
     return 0;
 }
 
