c078f42015cbd07365e34dd6e4db91095bcfaf3e
Czuberion <czuberion@gmail.com>
2024-05-05 16:12:04 +0200

    Translated everything to English Refactored variable names

--- lotnisko.c
+++ lotnisko.c
@@ -24,7 +24,7 @@
 typedef struct {
     pthread_mutex_t lock;
     pthread_cond_t condition;
-    int is_active; // Czy bramka jest aktywna (1) czy nie (0)
+    int is_active;
 } Gate;
 
 typedef struct {
@@ -36,11 +36,12 @@ Gate gates[GATE_COUNT];
 
 pthread_mutex_t global_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t global_cond = PTHREAD_COND_INITIALIZER;
-int in_personal_check = 0; // Czy którykolwiek pasażer jest obecnie na kontroli osobistej
-int multi_problem_flag = 0;
+int in_personal_check_count = 0;
+int multi_problem_flag = 0; // indicates that multiple personal checks 
+                            // have been triggered
 
 #ifdef DEBUG
-pthread_mutex_t prob_cnt_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t problem_count_lock = PTHREAD_MUTEX_INITIALIZER;
 int problem_count = 0;
 #endif
 
@@ -59,88 +60,88 @@ void* passenger_function(void* arg) {
     Gate* chosen_gate = &gates[chosen_gate_idx];
 
     pthread_mutex_lock(&chosen_gate->lock);
-    //DEBUG_PRINT("Pasażer %d, bramka %d: [L][S] Lock bramki.\n", id, chosen_gate_idx);
+    //DEBUG_PRINT("Passenger %d, Gate %d: [L][S] Lock gate.\n", id, chosen_gate_idx);
     while (!chosen_gate->is_active) {
-        DEBUG_PRINT("Pasażer %d, bramka %d: [W] czekanie na gate\n", id, chosen_gate_idx);
+        DEBUG_PRINT("Passenger %d, Gate %d: [W] Waiting for gate\n", id, chosen_gate_idx);
         pthread_cond_wait(&chosen_gate->condition, &chosen_gate->lock);
     }
 
     pthread_mutex_lock(&global_lock);
-    //DEBUG_PRINT("Pasażer %d, bramka %d: [L][S] Lock global.\n", id, chosen_gate_idx);
-    while(in_personal_check > 0 && multi_problem_flag) {
-        DEBUG_PRINT("Pasażer %d, bramka %d: [W] czekanie na global\n", id, chosen_gate_idx);
+    //DEBUG_PRINT("Passenger %d, Gate %d: [L][S] Lock global.\n", id, chosen_gate_idx);
+    while(in_personal_check_count > 0 && multi_problem_flag) {
+        DEBUG_PRINT("Passenger %d, Gate %d: [W] Waiting for global\n", id, chosen_gate_idx);
         pthread_cond_wait(&global_cond, &global_lock);
     }
 
-    DEBUG_PRINT("Pasażer %d, bramka %d: Podejście.\n", id, chosen_gate_idx);
+    DEBUG_PRINT("Passenger %d, Gate %d: Approach gate.\n", id, chosen_gate_idx);
 
     if (IS_PROBLEM) {
         chosen_gate->is_active = 0;
 
         #ifdef DEBUG
-        pthread_mutex_lock(&prob_cnt_lock);
+        pthread_mutex_lock(&problem_count_lock);
         problem_count++;
         int this_prob = problem_count;
-        pthread_mutex_unlock(&prob_cnt_lock);
+        pthread_mutex_unlock(&problem_count_lock);
         #endif
 
-        DEBUG_PRINT("Pasażer %d, bramka %d: [P%d][START] Problem przy prześwietlaniu.\n", id, chosen_gate_idx, this_prob);
-        if (in_personal_check > 0 && !multi_problem_flag) {
+        DEBUG_PRINT("Passenger %d, Gate %d: [P%d][START] Problem detected.\n", id, chosen_gate_idx, this_prob);
+        if (in_personal_check_count > 0 && !multi_problem_flag) {
             multi_problem_flag = 1;
-            DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K>1][S] Już ktoś na kontroli. Wstrz. bram.\n", id, chosen_gate_idx, this_prob);
+            DEBUG_PRINT("Passenger %d, Gate %d: [P%d->C>1][S] Someone is already being checked. Halting gates.\n", id, chosen_gate_idx, this_prob);
 
             for (int i = 0; i < GATE_COUNT; i++) {
                 gates[i].is_active = 0;
-                DEBUG_PRINT("Pasażer %d, bramka %d: Wstrz. bram. %d\n", id, chosen_gate_idx, i);
+                DEBUG_PRINT("Passenger %d, Gate %d: Halting gate #%d\n", id, chosen_gate_idx, i);
             }
-            DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K>1][S] Wstrz. wszyst. bram. zakończone.\n", id, chosen_gate_idx, this_prob);
+            DEBUG_PRINT("Passenger %d, Gate %d: [P%d->C>1][S] All gates halted.\n", id, chosen_gate_idx, this_prob);
         }
-        in_personal_check++;
+        in_personal_check_count++;
 
         pthread_cond_broadcast(&global_cond);
-        //DEBUG_PRINT("Pasażer %d, bramka %d: [L][E] Unlock global.\n", id, chosen_gate_idx);
+        //DEBUG_PRINT("Passenger %d, Gate %d: [L][E] Unlock global.\n", id, chosen_gate_idx);
         pthread_mutex_unlock(&global_lock);
 
         // Kontrola osobista
-        DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K][S] Początek kontroli.\n", id, chosen_gate_idx, this_prob);
+        DEBUG_PRINT("Passenger %d, Gate %d: [P%d->C][S] Personal check start.\n", id, chosen_gate_idx, this_prob);
         pthread_t check;
         pthread_create(&check, NULL, personal_check, &seed);
         pthread_join(check, NULL);
-        DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K][E] Koniec kontroli.\n", id, chosen_gate_idx, this_prob);
+        DEBUG_PRINT("Passenger %d, Gate %d: [P%d->C][E] Personal check end.\n", id, chosen_gate_idx, this_prob);
 
         pthread_mutex_lock(&global_lock);
-        //DEBUG_PRINT("Pasażer %d, bramka %d: [L][S] Lock global.\n", id, chosen_gate_idx);
-        in_personal_check--;
+        //DEBUG_PRINT("Passenger %d, Gate %d: [L][S] Lock global.\n", id, chosen_gate_idx);
+        in_personal_check_count--;
 
         if (IS_PROBLEM_2ND) {
-            DEBUG_PRINT("Pasażer %d, bramka %d: Kontrola osobista wykryła problem. Pasażer nie wpuszczony na pokład.\n", id, chosen_gate_idx);
+            DEBUG_PRINT("Passenger %d, Gate %d: Personal check has identified a problem. Passenger detained.\n", id, chosen_gate_idx);
         } else {
-            DEBUG_PRINT("Pasażer %d, bramka %d: Kontrola osobista nie wykryła problemu. Pasażer wpuszczony na pokład.\n", id, chosen_gate_idx);
+            DEBUG_PRINT("Passenger %d, Gate %d: Personal check hasn't identified any problem. Passenger boards the plane.\n", id, chosen_gate_idx);
         }
-        if (in_personal_check == 0 && multi_problem_flag) {
-            DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K>1][E] Nie ma więcej kontroli. Aktywacja bramek.\n", id, chosen_gate_idx, this_prob);
+        if (in_personal_check_count == 0 && multi_problem_flag) {
+            DEBUG_PRINT("Passenger %d, Gate %d: [P%d->C>1][E] No more personal checks. Activating gates.\n", id, chosen_gate_idx, this_prob);
             for (int i = 0; i < GATE_COUNT; i++) {
                 if(&gates[i] != chosen_gate) {
                     gates[i].is_active = 1;
-                    DEBUG_PRINT("Pasażer %d, bramka %d: Aktyw. bram. %d\n", id, chosen_gate_idx, i);
+                    DEBUG_PRINT("Passenger %d, Gate %d: Activating gate #%d\n", id, chosen_gate_idx, i);
                     pthread_cond_signal(&gates[i].condition);
                 }
             }
-            DEBUG_PRINT("Pasażer %d, bramka %d: Aktyw. bram. %d\n", id, chosen_gate_idx, chosen_gate_idx);
-            DEBUG_PRINT("Pasażer %d, bramka %d: [P%d->K>1][E] Aktyw. wszyst. bram. zakończone.\n", id, chosen_gate_idx, this_prob);
+            DEBUG_PRINT("Passenger %d, Gate %d: Activating gate #%d\n", id, chosen_gate_idx, chosen_gate_idx);
+            DEBUG_PRINT("Passenger %d, Gate %d: [P%d->C>1][E] All gates activated.\n", id, chosen_gate_idx, this_prob);
             multi_problem_flag = 0;
         }
         chosen_gate->is_active = 1;
-        DEBUG_PRINT("Pasażer %d, bramka %d: [P%d][END] Koniec problemów.\n", id, chosen_gate_idx, this_prob);
+        DEBUG_PRINT("Passenger %d, Gate %d: [P%d][END] Problem ended.\n", id, chosen_gate_idx, this_prob);
     } else {
-        DEBUG_PRINT("Pasażer %d, bramka %d: Przejście bez problemów.\n", id, chosen_gate_idx);
+        DEBUG_PRINT("Passenger %d, Gate %d: No problem detected. Passenger boards the plane.\n", id, chosen_gate_idx);
     }
 
     pthread_cond_signal(&chosen_gate->condition);
     pthread_cond_broadcast(&global_cond);
-    //DEBUG_PRINT("Pasażer %d, bramka %d: [L][E] Unlock bramki.\n", id, chosen_gate_idx);
+    //DEBUG_PRINT("Passenger %d, Gate %d: [L][E] Unlock gate.\n", id, chosen_gate_idx);
     pthread_mutex_unlock(&chosen_gate->lock);
-    //DEBUG_PRINT("Pasażer %d, bramka %d: [L][E] Unlock global.\n", id, chosen_gate_idx);
+    //DEBUG_PRINT("Passenger %d, Gate %d: [L][E] Unlock global.\n", id, chosen_gate_idx);
     pthread_mutex_unlock(&global_lock);
 
     return NULL;
@@ -183,7 +184,7 @@ int main() {
     pthread_mutex_destroy(&global_lock);
     pthread_cond_destroy(&global_cond);
     #ifdef DEBUG
-    pthread_mutex_destroy(&prob_cnt_lock);
+    pthread_mutex_destroy(&problem_count_lock);
     #endif
     return 0;
 }
